d9f5ffc33298b3e46327a4276edd3e5445ca546b
Matthew Beerens <mtk2011@live.com.au>
2022-01-11 20:26:27 +1100

    implement basic relational db and models

--- teammanager/bugtracker/admin.py
+++ teammanager/bugtracker/admin.py
@@ -2,7 +2,23 @@ from django.contrib import admin
 from .models import Project
 from .models import Bug
 from .models import Activity
+from .models import Position
+from .models import User
+from .models import Status
+from .models import Flag
+from .models import Employment
+from .models import Assignment
+from .models import Report
+from .models import BugAssign
 # Register your models here.
 admin.site.register(Project)
 admin.site.register(Bug)
 admin.site.register(Activity)
+admin.site.register(Position)
+admin.site.register(User)
+admin.site.register(Status)
+admin.site.register(Flag)
+admin.site.register(Employment)
+admin.site.register(Assignment)
+admin.site.register(Report)
+admin.site.register(BugAssign)
--- teammanager/bugtracker/models.py
+++ teammanager/bugtracker/models.py
@@ -1,16 +1,49 @@
 from django.db import models
 
 # Create your models here.
+class Position(models.Model):
+    title = models.CharField(max_length=60)
+
+class User(models.Model):
+    name = models.CharField(max_length=30)
+    lastname = models.CharField(max_length=30)
+    email = models.EmailField()
+    phone = models.CharField(max_length=12)
+
 class Project(models.Model):
     name = models.CharField(max_length=30)
 
-class Bug(models.Model):
-    title = models.CharField(max_length=50)
-    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+class Status(models.Model):
+    status = models.CharField(max_length=20)
+
+class Flag(models.Model):
+    flag = models.IntegerField()
+
+class Employment(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    position_id = models.ForeignKey(Position, on_delete=models.CASCADE)
+
+class Assignment(models.Model):
+    project_id = models.ForeignKey(Project, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Report(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    datetime = models.DateTimeField("date published")
+    tite = models.CharField(max_length=100)
     description = models.TextField()
-    date = models.DateTimeField('date published')
+
+class Bug(models.Model):
+    project_id = models.ForeignKey(Project, on_delete=models.CASCADE)
+    report_id = models.ForeignKey(Report, on_delete=models.CASCADE)
+    status_id = models.ForeignKey(Status, on_delete=models.PROTECT)
+    flag_id = models.ForeignKey(Flag, on_delete=models.PROTECT)
 
 class Activity(models.Model):
-    datetime = models.DateTimeField()
-    acitvity_description = models.CharField(max_length=30)
-    user_name = models.CharField(max_length=50)
+    user_id = models.ForeignKey(User, on_delete=models.PROTECT)
+    bug_id = models.ForeignKey(Bug, on_delete=models.PROTECT)
+    description = models.CharField(max_length=30)
+
+class BugAssign(models.Model):
+    bug_id = models.ForeignKey(Bug, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
