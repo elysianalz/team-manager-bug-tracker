35bafb04bc49b022bf09690124af3de4fcba35ca
Matthew Beerens <mtk2011@live.com.au>
2022-01-14 20:23:23 +1100

    add reportbug route

--- teammanager/bugtracker/urls.py
+++ teammanager/bugtracker/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path("", views.IndexView.as_view()),
     path("projects/", views.ProjectsView.as_view()),
     path("projects/<int:pk>/", views.ProjectView.as_view()),
+    path("projects/<int:pk>/reportbug/", views.ReportBugView.as_view()),
     path("login/", views.LoginView.as_view()),
 ]
